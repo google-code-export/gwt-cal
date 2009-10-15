a8e8b4a8988d94184838a39911c44a8742cb7d32
carlos.morales.duran <carlos.morales.duran@4ae8f392-6060-11de-bc6d-35d2ebd5fd4e>
2009-10-15 22:55:45 +0000

    Small Javadoc change to verify I am able to commit.

--- branches/version-0.9/gwt-cal/src/main/java/com/bradrydzewski/gwt/calendar/client/event/RollbackException.java
+++ branches/version-0.9/gwt-cal/src/main/java/com/bradrydzewski/gwt/calendar/client/event/RollbackException.java
@@ -3,8 +3,8 @@ package com.bradrydzewski.gwt.calendar.client.event;
 /**
  * <code>RollbackException</code> can be thrown to rollback or cancel
  * any changes made and not yet committed at the time of an Event.
- * <p>
- * An example is when an Appointment is deleted by the end-user.
+ * <p></p>
+ * An example is when an {@link com.bradrydzewski.gwt.calendar.client.Appointment} is deleted by the end-user.
  * A DeleteEvent is raised and the change can be reversed by throwing
  * the RollbackException.
  * 
@@ -12,7 +12,7 @@ package com.bradrydzewski.gwt.calendar.client.event;
  */
 public class RollbackException extends Exception {
   /**
-   * Default constructor.
+   * Default empty constructor.
    */
   public RollbackException() {
   }
