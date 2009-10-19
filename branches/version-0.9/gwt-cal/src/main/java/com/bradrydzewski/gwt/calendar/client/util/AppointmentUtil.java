ab97ac23848dc4e582ec1a57f6681c4aaa230466
carlos.morales.duran <carlos.morales.duran@4ae8f392-6060-11de-bc6d-35d2ebd5fd4e>
2009-10-19 01:27:58 +0000

    Added some javadoc comments to AppointmentUtil, still working on unit tests. Made code of rangeContains a little bit more compact. Won't really touch it until we have test cases -which I'm working on-. :)

--- branches/version-0.9/gwt-cal/src/main/java/com/bradrydzewski/gwt/calendar/client/util/AppointmentUtil.java
+++ branches/version-0.9/gwt-cal/src/main/java/com/bradrydzewski/gwt/calendar/client/util/AppointmentUtil.java
@@ -10,142 +10,162 @@ import com.google.gwt.user.client.DOM;
 import com.google.gwt.user.client.Element;
 
 /**
+ * Utility class for several operations involving time and {@link Appointment}
+ * objects.
  *
  * @author Brad Rydzewski
+ * @author Carlos D. Morales
  */
-public class AppointmentUtil {
-
-    
-    public static List<Date> findDatesOccupied(Date from, Date to, Date in) {
-        
+public class AppointmentUtil
+{
+    public static List<Date> findDatesOccupied(Date from, Date to, Date in)
+    {
         return null;
     }
-    
-    public static boolean isWithinRange(Date from, Date to, Date in) {
-        
-        Long apptStartLong = in.getTime();
-        
-        Long rangeStartLong = from.getTime();
-        Long rangeEndLong = to.getTime();
-        
-        if(apptStartLong >= rangeStartLong) {
-            if(apptStartLong <= rangeEndLong) {
-                return true;
-            }
-        }
-        
-        if(apptStartLong <= rangeStartLong) {
-            if(apptStartLong >= rangeStartLong) {
-                return true;
-            } 
-        }
-        
-        return false;
+
+    /**
+     * Provides a <code>null</code>-safe way to return the number of millisecons
+     * on a <code>date</code>.
+     *
+     * @param date The date whose value in milliseconds will be returned
+     * @return The number of milliseconds in <code>date</code>, <code>0</code>
+     *         (zero) if <code>date</code> is <code>null</code>.
+     */
+    private static long safeInMillis(Date date)
+    {
+        return date != null ? date.getTime() : 0;
     }
-    
-    public static boolean isMultiDay(Appointment appt) {
-        
-        if(appt.getStart().getDate() == appt.getEnd().getDay()) {
-            if(appt.getStart().getMonth() == appt.getEnd().getMonth()){
-                if(appt.getStart().getYear() == appt.getEnd().getYear()){
+
+    /**
+     * Tells whether the passed <code>testDate</code> falls within the time
+     * range defined by <code>rangeStart</code> and <code>rangeFrom</code>.
+     *
+     * @param rangeStart The range lower limit
+     * @param rangeEnd   The range upper limit
+     * @param testDate   The date to test
+     * @return <code>true</code> if <code>testDate</code> falls within the time
+     *         range defined by <code>rangeStart</code> and
+     *         <code>rangeEnd</code>
+     */
+    public static boolean isWithinRange(Date rangeStart, Date rangeEnd,
+       Date testDate)
+    {
+        Long testDateMillis = safeInMillis(testDate);
+        Long rangeStartMillis = safeInMillis(rangeStart);
+        Long rangeEndMillis = safeInMillis(rangeEnd);
+
+        return (testDateMillis >= rangeStartMillis
+           && testDateMillis <= rangeEndMillis);
+    }
+
+    public static boolean isMultiDay(Appointment appt)
+    {
+        if (appt.getStart().getDate() == appt.getEnd().getDay())
+        {
+            if (appt.getStart().getMonth() == appt.getEnd().getMonth())
+            {
+                if (appt.getStart().getYear() == appt.getEnd().getYear())
+                {
                     return false;
                 }
             }
         }
-        
+
         return false;
     }
-    
-    public static ArrayList filterListByDateRange(ArrayList fullList, Date date, int days) {
-        
+
+    public static ArrayList filterListByDateRange(ArrayList fullList, Date date,
+       int days)
+    {
         ArrayList<Appointment> group = new ArrayList<Appointment>();
-        Date startDate = (Date)date.clone();
+        Date startDate = (Date) date.clone();
         resetTime(startDate);
-        Date endDate = (Date)date.clone();
+        Date endDate = (Date) date.clone();
         resetTime(endDate);
         endDate.setDate(endDate.getDate() + days);
-        
-        for (int i = 0; i < fullList.size(); i++) {
 
-            Appointment tmpAppt = (Appointment) fullList.get(i);
-            if(tmpAppt.isMultiDay() && rangeContains(tmpAppt,startDate,endDate)) {
-                group.add(tmpAppt);
+        for (int i = 0; i < fullList.size(); i++)
+        {
+            Appointment appointment = (Appointment) fullList.get(i);
+            if (appointment.isMultiDay() && rangeContains(appointment,
+               startDate,
+               endDate))
+            {
+                group.add(appointment);
             }
         }
-        
+
         return group;
     }
-    
-    public static boolean rangeContains(Appointment appt, Date date) {
-        Date rangeStart = date;
-        Date rangeEnd = (Date)date.clone();
-        rangeEnd.setDate(rangeEnd.getDate()+1);
+
+    public static boolean rangeContains(Appointment appt, Date date)
+    {
+        Date rangeEnd = (Date) date.clone();
+        rangeEnd.setDate(rangeEnd.getDate() + 1);
         resetTime(rangeEnd);
-        return rangeContains(appt, rangeStart, rangeEnd);
+        return rangeContains(appt, date, rangeEnd);
     }
-    
+
     /**
-     * Determines if the given appointment intersects with the provided
-     * date range.
-     * @param appt
-     * @param rangeStart
-     * @param rangeEnd
-     * @return true false indicating appointment intersects with date range
+     * Indicates whether the specified <code>appointment</code> falls within the
+     * date range defined by <code>rangeStart</code> and <code>rangeEnd</code>.
+     *
+     * @param appointment The appointment to test
+     * @param rangeStart  The range lower limit
+     * @param rangeEnd    The range upper limit
+     * @return <code>true</code> if the appointment's date falls within the
+     *         range, <code>false</code> otherwise.
      */
-    public static boolean rangeContains(Appointment appt, Date rangeStart, Date rangeEnd) {
-        
-        Long apptStartLong = appt.getStart().getTime();
-        Long apptEndLong = appt.getEnd().getTime();
-        Long rangeStartLong = rangeStart.getTime();
-        Long rangeEndLong = rangeEnd.getTime();
-        
-        if(apptStartLong >= rangeStartLong) {
-            if(apptStartLong < rangeEndLong) {
-                return true;
-            }
-        }
-        
-        if(apptStartLong <= rangeStartLong) {
-            if(apptEndLong >= rangeStartLong) {
-                return true;
-            } 
-        }
-        
-        return false;
+    public static boolean rangeContains(Appointment appointment,
+       Date rangeStart, Date rangeEnd)
+    {
+        Long apptStartMillis = appointment.getStart().getTime();
+        Long apptEndMillis = appointment.getEnd().getTime();
+        Long rangeStartMillis = rangeStart.getTime();
+        Long rangeEndMillis = rangeEnd.getTime();
+
+        return apptStartMillis >= rangeStartMillis
+           && apptStartMillis < rangeEndMillis
+           || apptStartMillis <= rangeStartMillis
+           && apptEndMillis >= rangeStartMillis;
     }
-    
+
 
     /**
      * filters a list of appointments and returns only appointments with a start
      * date equal to the date provided. FYI - I hate everything about this
      * method and am pissed off I have to use it. May be able to avoid it in the
      * future
-     * 
+     *
      * @param fullList
      * @param startDate
      * @return
      */
-    public static ArrayList filterListByDate(List fullList, Date startDate) {
+    public static ArrayList filterListByDate(List fullList, Date startDate)
+    {
 
         ArrayList<Appointment> group = new ArrayList<Appointment>();
         startDate = new Date(startDate.getYear(), startDate.getMonth(),
-                startDate.getDate(), 0, 0, 0);
+           startDate.getDate(), 0, 0, 0);
         Date endDate = new Date(startDate.getYear(), startDate.getMonth(),
-                startDate.getDate(), 0, 0, 0);
+           startDate.getDate(), 0, 0, 0);
         endDate.setDate(endDate.getDate() + 1);
 
-        for (int i = 0; i < fullList.size(); i++) {
-
+        for (int i = 0; i < fullList.size(); i++)
+        {
             Appointment tmpAppt = (Appointment) fullList.get(i);
 
-            if (tmpAppt.isMultiDay()==false && tmpAppt.getEnd().before(endDate)) {
-
+            if (tmpAppt.isMultiDay() == false && tmpAppt.getEnd()
+               .before(endDate))
+            {
                 //TODO: probably can shorten this by using the compareTo method
-                
-                
-                if (tmpAppt.getStart().after(startDate) || tmpAppt.getStart().equals(startDate)) {
+                if (tmpAppt.getStart().after(startDate) || tmpAppt.getStart()
+                   .equals(startDate))
+                {
                     group.add(tmpAppt);
-                } else {
+                }
+                else
+                {
                     // System.out.println(" exiting filter at index " +i+ " ,"
                     // +group.size()+ " found");
                     // return group;
@@ -156,34 +176,34 @@ public class AppointmentUtil {
         return group;
     }
 
-    public static AppointmentWidget checkAppointmentElementClicked(Element element,List<AppointmentWidget> appointments) {
-        for (AppointmentWidget appt : appointments) {
+    public static AppointmentWidget checkAppointmentElementClicked(
+       Element element, List<AppointmentWidget> appointments)
+    {
+        for (AppointmentWidget appt : appointments)
+        {
             //throw new RuntimeException("checkAppointmentElementClicked is not implemented");
-            
-            if ( DOM.isOrHasChild(appt.getElement(), element) ) {
-                
+            if (DOM.isOrHasChild(appt.getElement(), element))
+            {
                 return appt;
             }
         }
-        
         return null;
     }
-    
-    
-    /**
-     * Resets the date to have no time modifiers.
-     * 
-     * @param date the date
-     */
-    public static void resetTime(Date date) {
-    	
-      long msec = date.getTime();
-      msec = (msec / 1000) * 1000;
-      date.setTime(msec);
 
-      date.setHours(0);
-      date.setMinutes(0);
-      date.setSeconds(0);
+
+    /**
+     * Resets the date to have no time modifiers (hours, minutes, seconds.)
+     *
+     * @param date The date to reset
+     */
+    public static void resetTime(Date date)
+    {
+        long msec = safeInMillis(date);
+        msec = (msec / 1000) * 1000;
+        date.setTime(msec);
+        date.setHours(0);
+        date.setMinutes(0);
+        date.setSeconds(0);
     }
 }
 
