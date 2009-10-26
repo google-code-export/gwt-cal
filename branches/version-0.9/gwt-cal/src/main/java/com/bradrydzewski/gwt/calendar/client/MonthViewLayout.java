49f4e4c4b54dc903191893999a773ab9d328c60d
carlos.morales.duran <carlos.morales.duran@4ae8f392-6060-11de-bc6d-35d2ebd5fd4e>
2009-10-26 16:33:01 +0000

    Removed commented lines. Moved the dayDiff method to AppointmentUtil.differenceInDays

--- branches/version-0.9/gwt-cal/src/main/java/com/bradrydzewski/gwt/calendar/client/MonthViewLayout.java
+++ branches/version-0.9/gwt-cal/src/main/java/com/bradrydzewski/gwt/calendar/client/MonthViewLayout.java
@@ -25,10 +25,13 @@ import java.util.List;
 import java.util.Map;
 
 import com.google.gwt.user.client.ui.Panel;
+import static com.bradrydzewski.gwt.calendar.client.util.AppointmentUtil.differenceInDays;
 
 public class MonthViewLayout {
 
-    class Coordinate {
+    private static final int HORIZONTAL_DAYS = 7;
+
+    static class Coordinate {
 
         private int x;
         private int y;
@@ -114,9 +117,9 @@ public class MonthViewLayout {
             return "row: " + row + ", col start: " + columnStart + ", col stop: " + columnStop + ", order: " + order;
         }
     }
-    private Map<Integer, Integer> cellsAppointmentCount = new HashMap<Integer, Integer>();
-    //private Map<Integer, List<AppointmentAdapter>> cellsAppointments = new HashMap<Integer, List<AppointmentAdapter>>();
-    private Map<Integer, Map<Integer,Integer>> cellsSlots = new HashMap<Integer, Map<Integer,Integer>>();
+    private HashMap<Integer, Integer> cellsAppointmentCount = new HashMap<Integer, Integer>();
+
+    private HashMap<Integer, Map<Integer,Integer>> cellsSlots = new HashMap<Integer, Map<Integer,Integer>>();
     
     public List<AppointmentAdapter> doLayout(List<Appointment> appointments,
             Date startDate, Date endDate) {
@@ -129,36 +132,25 @@ public class MonthViewLayout {
 
         //go through each appointment to place it on the screen
         for (Appointment appt : appointments) {
-                
                 List<AppointmentAdapter> tmpAdapterList = getAppointmentAdapter(appt,startDate);
-                
-                //System.out.println(appt.getTitle());
                 
                 for(AppointmentAdapter adapter : tmpAdapterList) {
                     int order = calculateOrder(adapter);
                     adapter.setOrder(order);
-                    
-                    //System.out.println("  " + adapter);
                 }
                 
                 adapters.addAll(tmpAdapterList);
-
-                
             }
-
-        
 
         return adapters;
     }
 
-    int calculateOrder(AppointmentAdapter adapter) {
-        
+    private int calculateOrder(AppointmentAdapter adapter) {
         //check each cell to get the max adapter, start w/ 0 and increment until available
-        int offset = adapter.getRow()*7;
+        int offset = adapter.getRow() * HORIZONTAL_DAYS;
         int start = adapter.getColumnStart()+offset;
         int stop = adapter.getColumnStop()+offset;
         int optimalSlot = 0;
-        
         
         for(int i=start;i<=stop;i++) {
             //get list of appointments already in the cell
@@ -180,14 +172,12 @@ public class MonthViewLayout {
         
         for(int i=start;i<=stop;i++) {
             cellsSlots.get(i).put(optimalSlot, optimalSlot);
-            //cellsAppointmentCount.put(i,)
         }
         
         return optimalSlot;
     }
     
     List<AppointmentAdapter> getAppointmentAdapter(Appointment appt, Date startDate) {
-
         List<AppointmentAdapter> adapters = new ArrayList<AppointmentAdapter>();
 
         //get the x,y coordinates for the cell the appointment will
@@ -200,7 +190,6 @@ public class MonthViewLayout {
         // a panel (gwt panel) must be created to represent the appointment.
         // for each panel we will have an adapter that plots it on the screen.
         for (int i = startCoordinate.getX(); i <= endCoordinate.getX(); i++) {
-
             int startCell = 0;
             int endCell = 0;
 
@@ -228,9 +217,8 @@ public class MonthViewLayout {
     }
 
     Coordinate getGridCoordinate(Date evalDate, Date startDate) {
-
         Coordinate c = new Coordinate();
-        double cellNumber = dayDiff(evalDate, startDate);
+        double cellNumber = differenceInDays(evalDate, startDate);
 
         double row = Math.floor(cellNumber / 7d);
         double col = Math.floor(cellNumber % 7d); //7 days (columns) per row
@@ -239,20 +227,4 @@ public class MonthViewLayout {
         c.setY((int) col);
         return c;
     }
-
-    public static int dayDiff(Date endDate, Date startDate) {
-
-        return (int) Math.floor((endDate.getTime() - startDate.getTime()) / (1000 * 60 * 60 * 24));
-    }
-
-    public static void resetDate(Date date) {
-        long msec = date.getTime();
-        msec = (msec / 1000) * 1000;
-        date.setTime(msec);
-
-        date.setHours(0);
-        date.setMinutes(0);
-        date.setSeconds(0);
-    }
 }
-
