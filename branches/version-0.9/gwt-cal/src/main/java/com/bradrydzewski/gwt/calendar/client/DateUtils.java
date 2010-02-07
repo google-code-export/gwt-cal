949ef550e34442894ac6822b84551cb69d15be36
carlos.morales.duran <carlos.morales.duran@4ae8f392-6060-11de-bc6d-35d2ebd5fd4e>
2010-02-07 21:06:31 +0000

    Creating test cases for the Date Utilities. Most importantly, re-implementing the differenceInDays function so that it correctly calculates the difference (in days) between two specific dates regardless of hour differences. The scenario was found after trying to calculate the difference in days between 12/26/2010 - 01/03/2011 (8 days) and 04/25/2010 - 05/03/2010 (8 days) as well. The latter time range was generating 7 days, causing the appointments on all month days to be displayed one day before the date they had specified by client code.

--- branches/version-0.9/gwt-cal/src/main/java/com/bradrydzewski/gwt/calendar/client/DateUtils.java
+++ branches/version-0.9/gwt-cal/src/main/java/com/bradrydzewski/gwt/calendar/client/DateUtils.java
@@ -28,67 +28,116 @@ import java.util.Date;
  */
 public class DateUtils {
 
-    public static final long MILLIS_IN_A_DAY = 1000 * 60 * 60 * 24;
+   public static final long MILLIS_IN_A_DAY = 1000 * 60 * 60 * 24;
 
-    /**
-     * Provides a <code>null</code>-safe way to return the number of millisecons
-     * on a <code>date</code>.
-     *
-     * @param date The date whose value in milliseconds will be returned
-     * @return The number of milliseconds in <code>date</code>, <code>0</code>
-     *         (zero) if <code>date</code> is <code>null</code>.
-     */
-    private static long safeInMillis(Date date) {
-        return date != null ? date.getTime() : 0;
-    }
+   /**
+    * Provides a <code>null</code>-safe way to return the number of millisecons
+    * on a <code>date</code>.
+    *
+    * @param date The date whose value in milliseconds will be returned
+    * @return The number of milliseconds in <code>date</code>, <code>0</code>
+    *         (zero) if <code>date</code> is <code>null</code>.
+    */
+   private static long safeInMillis(Date date) {
+      return date != null ? date.getTime() : 0;
+   }
 
-    /**
-     * Returns the number of days between the passed dates.
-     *
-     * @param endDate   The upper limit of the date range
-     * @param startDate The lower limit of the date range
-     * @return The number of days between <code>endDate</code> and
-     *         <code>starDate</code> (inclusive)
-     */
-    public static int differenceInDays(Date endDate, Date startDate) {
-        return (int) Math.floor((endDate.getTime() - startDate.getTime()) /
-            MILLIS_IN_A_DAY);
-    }
+   /**
+    * Returns the number of days between the passed dates.
+    *
+    * @param endDate   The upper limit of the date range
+    * @param startDate The lower limit of the date range
+    * @return The number of days between <code>endDate</code> and
+    *         <code>starDate</code> (inclusive)
+    */
+   public static int differenceInDays(Date endDate, Date startDate) {
 
-    /**
-     * Moves a date <code>shift</code> days. A clone of <code>date</code> to
-     * prevent undesired object modifications.
-     *
-     * @param date  The date to shift
-     * @param shift The number of days to push the original <code>date</code>
-     *              <em>forward</em>
-     * @return A <em>new</em> date pushed <code>shift</code> days forward
-     */
-    public static Date shiftDate(Date date, int shift) {
-        Date result = (Date) date.clone();
-        result.setDate(date.getDate() + shift);
-        return result;
-    }
+      long difference = endDate.getTime() - startDate.getTime();
+      int differenceInDays = 0;
+      do
+      {
+         difference = difference - MILLIS_IN_A_DAY;
+         differenceInDays++;
+      } while(difference >= MILLIS_IN_A_DAY);
 
-    /**
-     * Resets the date to have no time modifiers (hours, minutes, seconds.)
-     *
-     * @param date The date to reset
-     */
-    @SuppressWarnings("deprecation")
-    public static void resetTime(Date date) {
-        long milliseconds = safeInMillis(date);
-        milliseconds = (milliseconds / 1000) * 1000;
-        date.setTime(milliseconds);
-        date.setHours(0);
-        date.setMinutes(0);
-        date.setSeconds(0);
-    }
+      double remainder = Math.ceil((double) difference / (double) MILLIS_IN_A_DAY);
+      return differenceInDays + (int)remainder;
+   }
 
-    @SuppressWarnings("deprecation")
-    public static boolean areOnTheSameDay(Date dateOne, Date dateTwo) {
-        return dateOne.getDate() == dateTwo.getDate() &&
-            dateOne.getMonth() == dateTwo.getMonth() &&
-            dateOne.getYear() == dateTwo.getYear();
-    }
+   /**
+    * Moves a date <code>shift</code> days. A clone of <code>date</code> to
+    * prevent undesired object modifications.
+    *
+    * @param date  The date to shift
+    * @param shift The number of days to push the original <code>date</code>
+    *              <em>forward</em>
+    * @return A <em>new</em> date pushed <code>shift</code> days forward
+    */
+   @SuppressWarnings("deprecation")
+   public static Date shiftDate(Date date, int shift) {
+      Date result = (Date) date.clone();
+      result.setDate(date.getDate() + shift);
+      return result;
+   }
+
+   /**
+    * Resets the date to have no time modifiers (hours, minutes, seconds.)
+    *
+    * @param date The date to reset
+    */
+   @SuppressWarnings("deprecation")
+   public static void resetTime(Date date) {
+      long milliseconds = safeInMillis(date);
+      milliseconds = (milliseconds / 1000) * 1000;
+      date.setTime(milliseconds);
+      date.setHours(0);
+      date.setMinutes(0);
+      date.setSeconds(0);
+   }
+
+   /**
+    * Indicates whether two dates are on the same date by comparing their day,
+    * month and year values. Time values such as hours and minutes are not
+    * considered in this comparison.
+    *
+    * @param dateOne The first date to test
+    * @param dateTwo The second date to test
+    * @return <code>true</code> if both dates have their <code>date</code>,
+    *         <code>month</code> and <code>year</code> properties with the
+    *         <em>exact</em> same values (whatever they are)
+    */
+   @SuppressWarnings("deprecation")
+   public static boolean areOnTheSameDay(Date dateOne, Date dateTwo) {
+      return dateOne.getDate() == dateTwo.getDate() &&
+         dateOne.getMonth() == dateTwo.getMonth() &&
+         dateOne.getYear() == dateTwo.getYear();
+   }
+
+   /**
+    * Returns a clone of the <code>anyDayInMonth</code> date set to the
+    * <em>first</em> day of whatever its month is.
+    *
+    * @param anyDayInMonth Any date on a month+year
+    * @return A clone of the <code>anyDayInMonth</code> date, representing the
+    *         first day of that same month and year
+    */
+   @SuppressWarnings("deprecation")
+   public static Date firstOfTheMonth(Date anyDayInMonth) {
+      Date first = (Date) anyDayInMonth.clone();
+      first.setDate(1);
+      return first;
+   }
+
+   /**
+    * Moves the date of the passed object to be one day after whatever date it
+    * has.
+    *
+    * @param date An object representing a date
+    * @return The day
+    */
+   @SuppressWarnings("deprecation")
+   public static Date moveOneDayForward(Date date) {
+      date.setDate(date.getDate() + 1);
+      return date;
+   }
 }
---
+++ branches/version-0.9/gwt-cal/src/test/java/com/bradrydzewski/gwt/calendar/client/DateUtilsTest.java
@@ -0,0 +1,88 @@
+/*
+ * This file is part of gwt-cal
+ * Copyright (C) 2010  Scottsdale Software LLC
+ *
+ * gwt-cal is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/
+ */
+package com.bradrydzewski.gwt.calendar.client;
+
+import org.junit.Test;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import static com.bradrydzewski.gwt.calendar.client.DateUtils.firstOfTheMonth;
+import static com.bradrydzewski.gwt.calendar.client.DateUtils.moveOneDayForward;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotSame;
+
+/**
+ * Test cases for the set of utilities to work with <code>java.util.Date</code>
+ * objects.
+ *
+ * @author Carlos D. Morales
+ */
+public class DateUtilsTest {
+
+   private DateFormat dateFormatter = new SimpleDateFormat("MM/dd/yyyy");
+
+   @Test
+   public void moveOneDayForward_WithinSameMonth() throws Exception {
+      assertEquals(date("01/31/2010"), moveOneDayForward(date("01/30/2010")));
+   }
+
+   @Test
+   public void moveOneDayForward_AcrossMonths() throws Exception {
+      assertEquals(date("02/01/2010"), moveOneDayForward(date("01/31/2010")));
+   }
+
+   @Test
+   public void moveOneDayForward_AcrossYears() throws Exception {
+      assertEquals(date("01/01/2011"), moveOneDayForward(date("12/31/2010")));
+   }
+
+   @Test
+   public void firstOfTheMonth_LastOfJan2010() throws Exception {
+      Date someDayInMonth = date("01/31/2010");
+      Date first = firstOfTheMonth(someDayInMonth);
+      assertEquals(date("01/01/2010"), first);
+      assertNotSame(first, someDayInMonth);
+   }
+
+   @Test
+   public void firstOfTheMonth_LastOfFeb2010() throws Exception {
+      Date someDayInMonth = date("02/28/2010");
+      Date first = firstOfTheMonth(someDayInMonth);
+      assertEquals(date("02/01/2010"), first);
+      assertNotSame(first, someDayInMonth);
+   }
+
+   @Test
+   public void differenceInDays_SunApr_20100425_MonMay_20100503() throws Exception {
+      assertEquals(8, DateUtils.differenceInDays(date("05/03/2010"), date("04/25/2010")));
+   }
+
+
+   @Test
+   public void differenceInDays_SunDec_20101226_MonMay_20110103() throws Exception {
+      assertEquals(8, DateUtils.differenceInDays(date("01/03/2011"), date("12/26/2010")));
+   }
+
+
+   public Date date(String dateString) throws Exception {
+      return dateFormatter.parse(dateString);
+   }
+
+}
