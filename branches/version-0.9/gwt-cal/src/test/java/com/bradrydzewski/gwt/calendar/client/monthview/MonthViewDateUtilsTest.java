3ab4e3fe4b18306c706dcdd87c10d7b4884d354d
carlos.morales.duran <carlos.morales.duran@4ae8f392-6060-11de-bc6d-35d2ebd5fd4e>
2010-03-16 03:44:01 +0000

    Moving MonthViewDateUtilsTest (previously MonthViewHelperTest) to the month view package for consistency.

--- branches/version-0.9/gwt-cal/src/test/java/com/bradrydzewski/gwt/calendar/client/monthview/MonthViewDateUtilsTest.java
+++ branches/version-0.9/gwt-cal/src/test/java/com/bradrydzewski/gwt/calendar/client/monthview/MonthViewDateUtilsTest.java
@@ -15,7 +15,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/
  */
-package com.bradrydzewski.gwt.calendar.client;
+package com.bradrydzewski.gwt.calendar.client.monthview;
 
 import org.junit.Test;
 
