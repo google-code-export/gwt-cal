9275cde5bc7fbd10ec847272725dafed19b32d04
carlos.morales.duran@gmail.com <carlos.morales.duran@gmail.com@4ae8f392-6060-11de-bc6d-35d2ebd5fd4e>
2011-02-01 00:52:55 +0000

    Bug Fix, Issue 80. Wrong field set in the setTimeFormat setter.

--- gwt-cal/src/main/java/com/bradrydzewski/gwt/calendar/client/CalendarFormat.java
+++ gwt-cal/src/main/java/com/bradrydzewski/gwt/calendar/client/CalendarFormat.java
@@ -1,3 +1,20 @@
+/*
+ * This file is part of gwt-cal
+ * Copyright (C) 2009-2011  Scottsdale Software LLC
+ * 
+ * gwt-cal is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/
+ */
 package com.bradrydzewski.gwt.calendar.client;
 
 import com.bradrydzewski.gwt.calendar.client.i18n.CalendarConstants;
@@ -155,7 +172,7 @@ public class CalendarFormat {
     * in {@link com.google.gwt.i18n.client.DateTimeFormat}
     */
    public void setTimeFormat(String formatPattern) {
-      dateFormat = DateTimeFormat.getFormat(formatPattern);
+	  timeFormat = DateTimeFormat.getFormat(formatPattern);
       generateHourLabels();      
    }
 
