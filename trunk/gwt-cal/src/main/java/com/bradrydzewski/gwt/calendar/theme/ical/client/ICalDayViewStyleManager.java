477d05f0a3b0394e0debedee1700a5d10a3346c1
carlos.morales.duran@gmail.com <carlos.morales.duran@gmail.com@4ae8f392-6060-11de-bc6d-35d2ebd5fd4e>
2011-03-09 11:02:02 +0000

    Issue 114 Bug Fix: Do not use a background image if none is specified for the theme/style.
    originally posted in http://groups.google.com/group/gwt-cal/browse_thread/thread/e060bbc29d0d6765
    
    I created a new type ThemeAppointmentStyle to abstract the shared behavior of the GoogleAppointmentStyle and ICalAppointmentStyle and use this abstraction in a template method in the DayViewStyleManager and MonthViewStyleManager classes. Two new methods must be overriden by the subclasses, in order to provide an actual implementation for the *ViewStyleManager class to use when styling a single appointment.

--- trunk/gwt-cal/src/main/java/com/bradrydzewski/gwt/calendar/theme/ical/client/ICalDayViewStyleManager.java
+++ trunk/gwt-cal/src/main/java/com/bradrydzewski/gwt/calendar/theme/ical/client/ICalDayViewStyleManager.java
@@ -1,68 +1,43 @@
+/*
+ * This file is part of gwt-cal
+ * Copyright (C) 2011  Scottsdale Software LLC
+ *
+ * gwt-cal is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/
+ */
+
 package com.bradrydzewski.gwt.calendar.theme.ical.client;
 
 import com.bradrydzewski.gwt.calendar.client.Appointment;
 import com.bradrydzewski.gwt.calendar.client.dayview.DayViewStyleManager;
-import com.bradrydzewski.gwt.calendar.client.dayview.AppointmentWidget;
-import com.google.gwt.user.client.DOM;
-import com.google.gwt.user.client.Element;
+import com.bradrydzewski.gwt.calendar.client.ThemeAppointmentStyle;
 
+/**
+ * iCal theme style manager for the day view.
+ *
+ * @author Brad Rydzewski
+ * @author Carlos D. Morales
+ */
 public class ICalDayViewStyleManager extends DayViewStyleManager {
 
-	private static final String APPOINTMENT_STYLE = "dv-appointment";
-	private static final String APPOINTMENT_STYLE_SELECTED = "-selected";
-	private static final String APPOINTMENT_STYLE_MULTIDAY = "-multiday";
 
-	public void applyStyle(AppointmentWidget widget, boolean selected) {
+    @Override
+    protected ThemeAppointmentStyle getDefaultViewAppointmentStyleForTheme() {
+        return ICalAppointmentTheme.DEFAULT;
+    }
 
-		// Extract the Appointment for later reference
-		Appointment appointment = widget.getAppointment();
-		// Extract the DOM Element for later reference
-		Element elem = widget.getElement();
-		Element bodyElem = widget.getBody().getElement();
-		Element headerElem = widget.getHeader().getElement();
-		// Is MultiDay?
-		boolean multiDay = appointment.isMultiDay() || appointment.isAllDay();
-
-		
-		//Lookup the style from the map
-		ICalAppointmentStyle style = (ICalAppointmentStyle)
-			ICalAppointmentTheme.STYLES.get(appointment.getStyle());
-		
-		//Determine Style Name
-		String styleName = APPOINTMENT_STYLE;
-		if(multiDay) styleName+=APPOINTMENT_STYLE_MULTIDAY;
-		if(selected) styleName+=APPOINTMENT_STYLE_SELECTED;
-		widget.setStylePrimaryName(styleName);
-		
-		//If no style is found, apply the default blue style
-		//TODO: need to check for a custom style
-		if(style==null)
-			style =  (ICalAppointmentStyle) ICalAppointmentTheme.DEFAULT;
-		
-		
-		if (multiDay)
-			DOM.setStyleAttribute(elem, "backgroundColor", style.getBackgroundHeader());
-		else
-			DOM.setStyleAttribute(elem, "backgroundColor", style.getBackground());
-
-		DOM.setStyleAttribute(elem, "borderColor", style.getBackgroundHeader());
-
-		DOM.setStyleAttribute(bodyElem, "color", style.getSelectedBorder());
-
-		DOM.setStyleAttribute(headerElem, "color", style.getHeaderText());
-
-		DOM.setStyleAttribute(headerElem, "backgroundColor",style.getBackgroundHeader());
-
-		if (multiDay)
-			return;
-
-		if (selected) {
-			DOM.setStyleAttribute(elem, "backgroundImage", "url("
-					+ style.getSelectedBackgroundImage() + ")");
-		} else {
-			DOM.setStyleAttribute(elem, "backgroundImage", "none");
-		}
-
-	}
-
+    @Override
+    protected ThemeAppointmentStyle getViewAppointmentStyleForTheme(Appointment appointment) {
+        return ICalAppointmentTheme.STYLES.get(appointment.getStyle());
+    }
 }
--- trunk/gwt-cal/src/main/java/com/bradrydzewski/gwt/calendar/theme/ical/client/ICalMonthViewStyleManager.java
+++ trunk/gwt-cal/src/main/java/com/bradrydzewski/gwt/calendar/theme/ical/client/ICalMonthViewStyleManager.java
@@ -1,58 +1,36 @@
+/*
+ * This file is part of gwt-cal
+ * Copyright (C) 2011  Scottsdale Software LLC
+ *
+ * gwt-cal is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/
+ */
+
 package com.bradrydzewski.gwt.calendar.theme.ical.client;
 
 import com.bradrydzewski.gwt.calendar.client.Appointment;
-import com.bradrydzewski.gwt.calendar.client.monthview.AppointmentWidget;
+import com.bradrydzewski.gwt.calendar.client.ThemeAppointmentStyle;
 import com.bradrydzewski.gwt.calendar.client.monthview.MonthViewStyleManager;
-import com.google.gwt.user.client.DOM;
-import com.google.gwt.user.client.Element;
 
 public class ICalMonthViewStyleManager extends MonthViewStyleManager {
 
-	private static final String APPOINTMENT_STYLE = "appointment";
-	private static final String APPOINTMENT_STYLE_SELECTED = "-selected";
-	private static final String APPOINTMENT_STYLE_MULTIDAY = "-multiday";
+    @Override
+    protected ThemeAppointmentStyle getDefaultViewAppointmentStyleForTheme() {
+        return ICalAppointmentTheme.DEFAULT;
+    }
 
-
-	public void applyStyle(AppointmentWidget widget, boolean selected) {
-		
-		//Extract the Appointment for later reference
-		Appointment appointment = widget.getAppointment();
-		//Extract the DOM Element for later reference
-		Element elem = widget.getElement();
-		//Is MultiDay?
-		boolean multiDay = appointment.isMultiDay() || appointment.isAllDay();
-		
-		//Lookup the style from the map
-		ICalAppointmentStyle style = (ICalAppointmentStyle)
-			ICalAppointmentTheme.STYLES.get(appointment.getStyle());
-		
-		//Determine Style Name
-		String styleName = APPOINTMENT_STYLE;
-		if(multiDay) styleName+=APPOINTMENT_STYLE_MULTIDAY;
-		if(selected) styleName+=APPOINTMENT_STYLE_SELECTED;
-		widget.setStylePrimaryName(styleName);
-		
-		//If no style is found, apply the default blue style
-		//TODO: need to check for a custom style
-		if(style==null)
-			style =  (ICalAppointmentStyle) ICalAppointmentTheme.DEFAULT;
-		
-		//Apply Single vs. Multi-day style
-		if (multiDay) {
-			
-			DOM.setStyleAttribute(elem, "backgroundColor", style.getBackground());
-			DOM.setStyleAttribute(elem, "borderColor", style.getBorder());
-
-		} else {
-			
-			DOM.setStyleAttribute(elem, "color", style.getSelectedBorder());
-		}
-
-		//Apply style specific to selected appointments
-		if (selected) {
-			
-			DOM.setStyleAttribute(elem, "borderColor", style.getSelectedBorder());
-		}
-	}
-
+    @Override
+    protected ThemeAppointmentStyle getViewAppointmentStyleForTheme(Appointment appointment) {
+        return ICalAppointmentTheme.STYLES.get(appointment.getStyle());
+    }
 }
